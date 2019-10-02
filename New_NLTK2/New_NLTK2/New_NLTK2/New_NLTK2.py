add55e3444b5e0c51e13a9bae9c979d3912a9b32
sophie-bailard <45054204+sophie-bailard@users.noreply.github.com>
2019-10-01 22:38:08 -0400

    Add files via upload

---
+++ New_NLTK2/New_NLTK2/New_NLTK2/New_NLTK2.py
@@ -0,0 +1,20 @@
+
+
+from nltk.stem import PorterStemmer
+
+class Test(PorterStemmer):
+    def __init__(self):
+        super().__init__()
+
+    def stem(self, word):
+        return super().stem(word)
+
+    def next(self):
+        return super().next()
+
+
+stemmer = PorterStemmer()
+testing = Test()
+ 
+print(stemmer.stem('working'))
+print(testing.stem('working'))
